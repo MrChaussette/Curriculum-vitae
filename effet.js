7d278495317c5f8e82497e5ebca01ed8133611fb
MrChaussette <106651770+MrChaussette@users.noreply.github.com>
2022-07-30 19:13:25 +0200

    maj 19h13

--- effet.js
+++ effet.js
@@ -1,2 +1,14 @@
-// 4 const getelementbyId des 4 DIV (txt_**) creer funtion if else-> if
-// mettre le html child id(txt_**) dans innerHTML pour generer le html avec le JS
+
+function skill(skill){
+    document.getElementById('txt').innerHTML = "<img class='floaticon' src='./icon/icon_skill.png' alt='Compétence'><p>Compétence:</p><br><ul><li><img src='./icon/icon_html.png' alt='HTML5'></li><li><img src='./icon/icon_css3.png' alt='CSS3'></li><li><img src='./icon/icon_js.png' alt='Javascript'></li></ul>";
+    
+}
+function experience(experience){
+    document.getElementById('txt').innerHTML = "<img class='floaticon' src='./icon/icon_exp.png' alt='Expérience'><p>Expérience:</p><br><ul><h5>Vendeur multimédia</h5><li><span class='bold' >Boulanger :</span> Saint-Quentin, Janvier 2022 à Février 2022</li></ul><ul><h5>Vendeur multimédia</h5><li><span class='bold' >Boulanger :</span> Saint-Quentin, Décembre 2021</li></ul><ul><h5>Agent d'Accueil polyvalent</h5><li><span class='bold' >Village des métriers d'antan & musée motobécane :</span> Depuis Juin 2021</li></ul><ul><h5>Vendeur multimédia</h5><li><span class='bold' >Darty :</span> Saint-Quentin, 2020</li></ul><ul><h5>Employé polyvalent menuiserie</h5><li><span class='bold' >A.I.P.S.Q - R.O.D.A :</span>Saint-Quentin, Janvier 2020 à Mai 2022</li></ul><ul><h5>Opérateur de fabrication</h5><li><span class='bold' >MBK Industrie :</span> Saint-Quentin, 2019</li></ul><ul><h5>Manutentionnaire</h5><li><span class='bold' >Ondulys :</span> Saint-Quentin, 2017</li></ul><ul><h5>Agent de sécurité</h5><li><span class='bold' >Leader Price :</span> Saint-Quentin, 2017 </li></ul><ul><h5></h5>Vendeur multimédia<li><span class='bold' >Cora :</span> Saint-Quentin, 2014</li></ul><ul><h5>Ebénisterie</h5><li><span class='bold' >Diverses entreprises :</span> Saint-Quentin, 2013</li></ul>";
+}
+function diplom(diplom){
+    document.getElementById('txt').innerHTML = "<img class='floaticon' src='./icon/icon_diplome.png' alt='Diplôme'><p>Diplômes et formations:</p><br><ul><li><span class='bold' >Certificat :</span> HTML 5 & CSS 3</li><br><li><span class='bold' >SST :</span> Sauveteur secouriste du travail</li><br><li><span class='bold' >CQPM :</span> Soudeur TIG</li><br><li><span class='bold' >CAP :</span> Sculpteur ornemaniste</li><br><li><span class='bold' >CAP :</span> Ebéniste</li></ul>"
+}
+function interet(interet){
+    document.getElementById('txt').innerHTML = "<img class='floaticon' src='./icon/icon_totoro.png' alt='Totoro'><p>Centre d'intêret :</p><br><ul><li><span class='bold' >Manga - animé</span></li><br><li><span class='bold'>Jeux vidéo</span></li><br><li><span class='bold' >Cyclisme</span></li><br></ul>"
+}
